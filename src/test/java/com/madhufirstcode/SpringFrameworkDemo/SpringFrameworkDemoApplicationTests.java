70e5f5abee530e0a96e536b00a3b5ad5e23d14bc
Madhavi <114247548+Madhu-02522@users.noreply.github.com>
2023-04-11 20:13:43 -0400

    Add files via upload

---
+++ src/test/java/com/madhufirstcode/SpringFrameworkDemo/SpringFrameworkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhufirstcode.SpringFrameworkDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
